939737d09bf0ce9e1001c02c2bccd01daf3d09f1
Fall <fallkhadim6800@gmail.com>
2023-12-30 11:33:07 +0100

    Ajout de la premiere version avec minimaxcomenté profondeur 5-->2

--- ProjetIAgit1/classes/strategy.py
+++ ProjetIAgit1/classes/strategy.py
@@ -2,7 +2,6 @@ import copy
 import math
 import random
 from math import log, sqrt, inf
-from random import randrange
 import numpy as np
 from rich.table import Table
 from rich.progress import track
@@ -11,46 +10,27 @@ from rich.progress import Progress
 
 import classes.logic as logic
 
-# When implementing a new strategy add it to the `str2strat`
-# dictionary at the end of the file
-
-
+# Base class for different player strategies in the game.
 class PlayerStrat:
     def __init__(self, _board_state, player):
+        """
+        Initialize the player strategy with the current state of the board and the player number.
+
+        :param _board_state: The current state of the board as a 2D list.
+        :param player: The player number (1 or 2).
+        """
         self.root_state = _board_state
         self.player = player
 
     def start(self):
         """
-        This function select a tile from the board.
+        Abstract method to select a tile from the board. To be implemented by subclasses.
 
-        @returns    (x, y) A tuple of integer corresponding to a valid
-                    and free tile on the board.
+        :returns: (x, y) tuple of integers corresponding to a valid and free tile on the board.
         """
         raise NotImplementedError
 
-class Node(object):
-    """
-    This class implements the main object that you will manipulate : nodes.
-    Nodes include the state of the game (i.e. the 2D board), children (i.e. other children nodes), a list of
-    untried moves, etc...
-    """
-    def __init__(self, board, move=(None, None),
-                 wins=0, visits=0, children=None):
-        # Save the #wins:#visited ratio
-        self.state = board
-        self.move = move
-        self.wins = wins
-        self.visits = visits
-        self.children = children or []
-        self.parent = None
-        self.untried_moves = logic.get_possible_moves(board)
-
-    def add_child(self, child):
-        child.parent = self
-        self.children.append(child)
-
-
+# Random strategy for a player. Chooses a move randomly from available tiles.
 class RandomPlayer(PlayerStrat):
     def __init__(self, _board_state, player):
         super().__init__(_board_state, player)
@@ -60,28 +40,37 @@ class RandomPlayer(PlayerStrat):
         """
         Randomly selects a free tile on the board.
 
-        :param board: the current game board
-        :returns: (x, y) a tuple of integers corresponding to a valid and free tile on the board.
+        :param board: The current game board.
+        :returns: (x, y) tuple of integers corresponding to a valid and free tile on the board.
         """
         free_tiles = [(x, y) for x in range(self.board_size) for y in range(self.board_size) if board[x][y] == 0]
         return random.choice(free_tiles) if free_tiles else None
-        def start(self):
-         return self.select_tile(self.root_state)
+
     def start(self):
-      return self.select_tile(self.root_state)
+        return self.select_tile(self.root_state)
 
-
-
-   
-    
-    # Définition de la classe MiniMax
+# MiniMax strategy for a player. Uses the MiniMax algorithm to choose the best move.
 class MiniMax(PlayerStrat):
     def __init__(self, _board_state, player, depth=2):
+        """
+        Initialize the MiniMax player with the current state of the board, player number, and search depth.
+
+        :param _board_state: The current state of the board as a 2D list.
+        :param player: The player number (1 or 2).
+        :param depth: The depth of search in the MiniMax algorithm.
+        """
         super().__init__(_board_state, player)
         self.board_size = len(_board_state)
         self.depth = depth
 
     def select_tile(self, board, player):
+        """
+        Selects the best move using the MiniMax strategy.
+
+        :param board: The current game board.
+        :param player: The player number for whom the move is being calculated.
+        :returns: (x, y) tuple of integers corresponding to the best move.
+        """
         best_score = float('-inf')
         best_move = None
 
@@ -98,6 +87,15 @@ class MiniMax(PlayerStrat):
         return best_move
 
     def minimax(self, board, depth, is_maximizing, player):
+        """
+        MiniMax algorithm to evaluate the board and return the best score.
+
+        :param board: The game board.
+        :param depth: Current depth of the search tree.
+        :param is_maximizing: Boolean indicating whether the current layer is maximizing or minimizing.
+        :param player: The player number.
+        :returns: The best score from the current board state.
+        """
         if depth == 0 or self.is_game_over(board):
             return self.evaluate_board(board, player)
 
@@ -123,25 +121,35 @@ class MiniMax(PlayerStrat):
             return best_score
 
     def is_game_over(self, board):
+        """
+        Check if the game is over.
+
+        :param board: The game board.
+        :returns: Boolean indicating whether the game is over.
+        """
         return logic.is_game_over(self.player, board) is not None
 
     def evaluate_board(self, board, player):
+        """
+        Evaluate the board and return a score based on the game state.
+
+        :param board: The game board.
+        :param player: The player number.
+        :returns: A score representing the state of the board.
+        """
         if logic.is_game_over(player, board):
-            return 10
+            return 10  # Win
         elif logic.is_game_over(3 - player, board):
-            return -10
+            return -10  # Loss
         else:
-            return 0
+            return 0  # Neutral
 
     def start(self):
         return self.select_tile(self.root_state, self.player)
 
-    
-
-
-   
+# Dictionary to map strategy names to their respective classes.
 str2strat: dict[str, PlayerStrat] = {
-        "human": None,
-        "random": RandomPlayer,
-        "minimax": MiniMax,
+    "human": None,
+    "random": RandomPlayer,
+    "minimax": MiniMax,
 }
